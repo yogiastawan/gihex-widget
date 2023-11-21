6aeed7e915f0268a67417495786eb78c43cb1dd7
Yogi Astawan <yogi.astawan@gmail.com>
2023-11-22 01:06:48 +0800

    fix  draw `gihex_gauge`

--- examples/gihex_gauge.rs
+++ examples/gihex_gauge.rs
@@ -1,8 +1,8 @@
 use gihex_widget::gihex_gauge::GihexGauge;
 use gtk4::{
     glib,
-    prelude::{ApplicationExt, ApplicationExtManual, GtkWindowExt},
-    Application, ApplicationWindow,
+    prelude::{ApplicationExt, ApplicationExtManual, BoxExt, GtkWindowExt},
+    Application, ApplicationWindow, Box,
 };
 
 fn main() -> glib::ExitCode {
@@ -16,13 +16,22 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
+    let container = Box::new(gtk4::Orientation::Horizontal, 20);
     let gauge = GihexGauge::new();
+    let gauge_1 = GihexGauge::new();
+    gauge_1.set_unit_value("\u{2103}");
+    gauge_1.set_value(60.0);
+    let gauge_2 = GihexGauge::new();
+    gauge_2.set_unit_value("\u{2109}");
+    container.append(&gauge);
+    container.append(&gauge_1);
+    container.append(&gauge_2);
     let win = ApplicationWindow::builder()
         .application(app)
         .title("Example Gihex Gauge")
-        .width_request(480)
-        .height_request(320)
-        .child(&gauge)
+        // .width_request(480)
+        // .height_request(320)
+        .child(&container)
         .build();
 
     win.present();
--- src/gihex_gauge.rs
+++ src/gihex_gauge.rs
@@ -2,7 +2,10 @@ use gtk4::glib;
 use gtk4::glib::Object;
 
 mod imp {
-    use std::cell::Cell;
+    use std::{
+        cell::{Cell, RefCell},
+        f64::consts::PI,
+    };
 
     use gtk4::{
         glib::{
@@ -29,6 +32,10 @@ mod imp {
         min_value: Cell<f32>,
         #[property(get, set,type=f32, name="max-value", default = 100f32)]
         max_value: Cell<f32>,
+        #[property(get, set,type=f32, name="unit-font-size", default = 12f32)]
+        unit_font_size: Cell<f32>,
+        #[property(get, set,type=String, name="unit-value", default = "%")]
+        unit: RefCell<String>,
     }
 
     #[glib::object_subclass]
@@ -36,6 +43,7 @@ mod imp {
         const NAME: &'static str = "GihexGauge";
         type Type = super::GihexGauge;
         type ParentType = Widget;
+        const ABSTRACT: bool = false;
 
         fn class_init(klass: &mut Self::Class) {
             klass.set_css_name("gx_gauge");
@@ -46,6 +54,8 @@ mod imp {
                 value: Cell::new(30f32),
                 min_value: Cell::new(0f32),
                 max_value: Cell::new(100f32),
+                unit_font_size: Cell::new(12.0),
+                unit: RefCell::new("%".to_owned()),
             }
         }
     }
@@ -56,15 +66,117 @@ mod imp {
     impl WidgetImpl for GihexGauge {
         fn snapshot(&self, snapshot: &gtk4::Snapshot) {
             let widget = self.obj();
+
             let w = widget.allocated_width();
             let h = widget.allocated_height();
+            let size = match w > h {
+                true => h as f64,
+                false => w as f64,
+            };
 
+            let ro = (size - 2.0) / 2.0;
+            let ri = (size - 2.0) / 3.0;
+
+            let max_value = self.max_value.get() as f64;
+            let min_value = self.min_value.get() as f64;
+            let value = self.value.get() as f64;
+            let value = if value > max_value {
+                max_value
+            } else if value < min_value {
+                min_value
+            } else {
+                value
+            };
+
+            let mut value_text = value.to_string();
+            value_text.push_str(self.unit.borrow().as_str());
+
+            //draw base gauge
             let cr = snapshot.append_cairo(&graphene::Rect::new(0f32, 0f32, w as f32, h as f32));
-
-            // cr.arc(xc, yc, radius, angle1, angle2)
+            cr.arc(size / 2.0, size / 2.0, ro, 2.0 * PI / 3.0, 7.0 * PI / 3.0);
+            cr.arc(
+                (size / 2.0) - (((ro + ri) / 2.0) * (11.0 * PI / 6.0).sin()),
+                (size / 2.0) + (((ro + ri) / 2.0) * (11.0 * PI / 6.0).cos()),
+                (ro - ri) / 2.0,
+                PI / 3.0,
+                4.0 * PI / 3.0,
+            );
+            cr.arc_negative(size / 2.0, size / 2.0, ri, PI / 3.0, 2.0 * PI / 3.0);
+            cr.arc(
+                (size / 2.0) - (((ro + ri) / 2.0) * (PI / 6.0).sin()),
+                (size / 2.0) + (((ro + ri) / 2.0) * (PI / 6.0).cos()),
+                (ro - ri) / 2.0,
+                5.0 * PI / 3.0,
+                2.0 * PI / 3.0,
+            );
+            cr.close_path();
             cr.set_source_rgb(0.0, 0.5, 0.5);
-            cr.arc(w as f64 / 2.0, h as f64 / 2.0, h as f64 / 2.0, 0.0, 360.0);
+            cr.fill_preserve().unwrap();
+            cr.set_source_rgb(0.0, 0.45, 0.45);
+            cr.stroke().unwrap();
+            cr.set_line_width(1.0);
+
+            //draw value
+            let cr = snapshot.append_cairo(&graphene::Rect::new(0f32, 0f32, w as f32, h as f32));
+            cr.arc(
+                size / 2.0,
+                size / 2.0,
+                ro - 2.0,
+                2.0 * PI / 3.0,
+                ((300.0 * (value - min_value) / (max_value - min_value)) + 120.0) * PI / 180.0,
+            );
+            cr.arc(
+                (size / 2.0)
+                    - (((ro + ri) / 2.0)
+                        * (((300.0 * (value - min_value) / (max_value - min_value)) + 30.0) * PI
+                            / 180.0)
+                            .sin()),
+                (size / 2.0)
+                    + (((ro + ri) / 2.0)
+                        * (((300.0 * (value - min_value) / (max_value - min_value)) + 30.0) * PI
+                            / 180.0)
+                            .cos()),
+                (ro - ri - 4.0) / 2.0,
+                (120.0 + (300.0 * (value - min_value) / (max_value - min_value))) * PI / 180.0,
+                (300.0 * (1.0 + ((value - min_value) / (max_value - min_value)))) * PI / 180.0,
+            );
+            cr.arc_negative(
+                size / 2.0,
+                size / 2.0,
+                ri + 2.0,
+                ((300.0 * (value - min_value) / (max_value - min_value)) + 120.0) * PI / 180.0,
+                2.0 * PI / 3.0,
+            );
+            cr.arc(
+                (size / 2.0) - (((ro + ri) / 2.0) * (PI / 6.).sin()),
+                (size / 2.0) + (((ro + ri) / 2.0) * (PI / 6.0).cos()),
+                (ro - ri - 4.0) / 2.0,
+                5.0 * PI / 3.0,
+                2.0 * PI / 3.0,
+            );
+            cr.close_path();
+            cr.set_source_rgb(0.0, 0.65, 0.8);
             cr.fill().unwrap();
+
+            //draw text value
+            let cr = snapshot.append_cairo(&graphene::Rect::new(0f32, 0f32, w as f32, h as f32));
+            cr.set_font_size(self.unit_font_size.get() as f64 * size / 75.0);
+            let text_extends = cr.text_extents(value_text.as_str()).unwrap();
+            cr.move_to(
+                (size / 2.0) - ((text_extends.width() / 2.0) + text_extends.x_bearing()),
+                (size / 2.0) - ((text_extends.height() / 2.0) + text_extends.y_bearing()),
+            );
+            cr.text_path(value_text.as_str());
+            cr.set_source_rgb(0.0, 0.5, 0.5);
+            cr.fill().unwrap();
+        }
+
+        // fn request_mode(&self) -> gtk4::SizeRequestMode {
+        //     gtk4::SizeRequestMode::ConstantSize
+        // }
+
+        fn measure(&self, _orientation: gtk4::Orientation, _for_size: i32) -> (i32, i32, i32, i32) {
+            (120, 300, -1, -1)
         }
     }
 }
