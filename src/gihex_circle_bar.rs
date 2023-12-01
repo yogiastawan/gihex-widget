02365349f934200b65783714f83ba42e6002b7db
Yogi Astawan <yogi.astawan@gmail.com>
2023-12-01 22:22:19 +0800

    fix drawing

--- src/gihex_circle_bar.rs
+++ src/gihex_circle_bar.rs
@@ -1,7 +1,10 @@
 use gtk4::glib::{self, Object};
 
 mod imp {
-    use std::{cell::Cell, f64::consts::PI};
+    use std::{
+        cell::{Cell, RefCell},
+        f64::consts::PI,
+    };
 
     use gtk4::{
         gdk::RGBA,
@@ -14,26 +17,46 @@ mod imp {
             },
             Properties,
         },
-        graphene::Rect,
+        graphene::{self, Rect},
         prelude::*,
         subclass::widget::{WidgetClassExt, WidgetImpl},
         Widget,
     };
+
+    use crate::gihex_gauge_bar::GihexColor;
 
     #[derive(Properties, Default)]
     #[properties(wrapper_type=super::GihexCircleBar)]
     pub struct GihexCircleBar {
         #[property(get,set=Self::set_value,type=f32, default = 30f32)]
         value: Cell<f32>,
+        #[property(get,set=Self::set_max_value,type=f32,name="max-value", default = 100f32)]
         max_value: Cell<f32>,
         min_value: Cell<f32>,
         numb_step: Cell<u8>,
-        current_index: Cell<u8>,
+        indeterminate: Cell<bool>,
+        current_index: Cell<i8>,
+        name: RefCell<String>,
+        unit: RefCell<String>,
+        font_size: Cell<f32>,
+        sub_font_size: Cell<f32>,
+        #[property(get,set=Self::set_background_color,type=GihexColor,name="background-color")]
+        background_color: RefCell<GihexColor>,
     }
 
     impl GihexCircleBar {
         fn set_value(&self, value: f32) {
             self.value.set(value);
+            self.obj().queue_draw();
+        }
+
+        fn set_max_value(&self, value: f32) {
+            self.max_value.set(value);
+            self.obj().queue_draw();
+        }
+
+        fn set_background_color(&self, value: GihexColor) {
+            *self.background_color.borrow_mut() = value;
             self.obj().queue_draw();
         }
     }
@@ -55,7 +78,13 @@ mod imp {
                 max_value: Cell::new(100.0),
                 min_value: Cell::new(0.0),
                 numb_step: Cell::new(20),
+                indeterminate: Cell::new(false),
                 current_index: Cell::new(0),
+                name: RefCell::new("Circle Bar".to_owned()),
+                unit: RefCell::new("%".to_owned()),
+                font_size: Cell::new(48.0),
+                sub_font_size: Cell::new(24.0),
+                background_color: RefCell::new(GihexColor::new(0, 0, 0, 255)),
             }
         }
     }
@@ -85,6 +114,8 @@ mod imp {
 
             let s_angle = (s_dash / r_o_dash).atan();
 
+            let s_y_word = 16.0 * size / 300.0;
+
             //draw background
             snapshot.append_color(
                 &RGBA::new(0.0, 0.2, 0.2, 1.0),
@@ -100,9 +131,13 @@ mod imp {
 
             //draw dash
             let cr = snapshot.append_cairo(&Rect::new(0.0, 0.0, size as f32, size as f32));
-            let id_val = (self.value.get() / (self.max_value.get() - self.min_value.get()))
-                * self.numb_step.get() as f32;
-            let id_val = id_val as i8 - 1;
+
+            if !self.indeterminate.get() {
+                let id_val = (self.value.get() / (self.max_value.get() - self.min_value.get()))
+                    * self.numb_step.get() as f32;
+                let id_val = id_val as i8 - 1;
+                self.current_index.set(id_val)
+            }
             let step_angle = 360 / self.numb_step.get() as i16;
             for i in 0..self.numb_step.get() as i16 {
                 cr.save().unwrap();
@@ -131,7 +166,7 @@ mod imp {
                     (((i * step_angle) as f64 - 90.0) * PI / 180.0) + s_angle,
                 );
                 cr.close_path();
-                if i <= id_val.into() {
+                if i <= self.current_index.get().into() {
                     cr.set_source_rgba(0.05, 0.627, 0.59, 1.0);
                 } else {
                     cr.set_source_rgba(0.7, 0.827, 0.827, 0.4);
@@ -139,6 +174,48 @@ mod imp {
                 cr.fill().unwrap();
                 cr.restore().unwrap();
             }
+
+            //draw text value
+            let value_text = format!("{:.1}", self.value.get());
+            let cr =
+                snapshot.append_cairo(&graphene::Rect::new(0.0, 0.0, size as f32, size as f32));
+            cr.set_font_size(self.font_size.get() as f64 * size / 300.0);
+            let text_extends = cr.text_extents(value_text.as_str()).unwrap();
+            cr.move_to(
+                (size / 2.0) - ((text_extends.width() / 2.0) + text_extends.x_bearing()),
+                (size / 2.0) - ((text_extends.height() / 2.0) + text_extends.y_bearing()),
+            );
+            cr.text_path(value_text.as_str());
+            cr.set_source_rgba(0.05, 0.627, 0.59, 1.0);
+            cr.fill().unwrap();
+
+            // draw unit
+            let cr =
+                snapshot.append_cairo(&graphene::Rect::new(0.0, 0.0, size as f32, size as f32));
+            cr.set_font_size(self.sub_font_size.get() as f64 * size / 300.0);
+            let unit_text_extends = cr.text_extents(self.unit.borrow().as_str()).unwrap();
+            cr.move_to(
+                (size / 2.0) - ((unit_text_extends.width() / 2.0) + unit_text_extends.x_bearing()),
+                (size / 2.0) + s_y_word + unit_text_extends.height()
+                    - ((text_extends.height() / 2.0) + text_extends.y_bearing()),
+            );
+            cr.text_path(self.unit.borrow().as_str());
+            cr.set_source_rgba(0.05, 0.627, 0.59, 1.0);
+            cr.fill().unwrap();
+
+            // draw name
+            let cr =
+                snapshot.append_cairo(&graphene::Rect::new(0.0, 0.0, size as f32, size as f32));
+            cr.set_font_size(self.sub_font_size.get() as f64 * size / 300.0);
+            let name_text_extends = cr.text_extents(self.name.borrow().as_str()).unwrap();
+            cr.move_to(
+                (size / 2.0) - ((name_text_extends.width() / 2.0) + name_text_extends.x_bearing()),
+                (size / 2.0) - s_y_word
+                    + ((text_extends.height() / 2.0) + text_extends.y_bearing()),
+            );
+            cr.text_path(self.name.borrow().as_str());
+            cr.set_source_rgba(0.05, 0.627, 0.59, 1.0);
+            cr.fill().unwrap();
         }
 
         fn measure(&self, _orientation: gtk4::Orientation, for_size: i32) -> (i32, i32, i32, i32) {
